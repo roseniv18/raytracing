81ceb2011339e371daf7443a12c884b0413277e7
roseniv18 <roseniv18@yahoo.com>
2024-04-27 21:15:23 +0300

    Comments

--- main.cpp
+++ main.cpp
@@ -22,12 +22,16 @@
 
 */
 double hit_sphere(const point3& center, double radius, const ray& r) {
+	// C - Q; C = sphere center, Q = r.origin() or ray origin point.
 	vec3 oc = center - r.origin();
+	// Terms of the quadratic equation.
 	auto a = dot(r.direction(), r.direction());
 	auto b = -2.0 * dot(r.direction(), oc);
 	auto c = dot(oc, oc) - radius * radius;
+
 	auto discriminant = b * b - 4 * a * c;
 
+	// Check if discriminant has real solutions.
 	if(discriminant < 0) {
 		return -1.0;
 	}
@@ -38,19 +42,24 @@ double hit_sphere(const point3& center, double radius, const ray& r) {
 }
 
 color ray_color(const ray& r) {
+	// t is the discriminant of the quadratic equation.
 	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
 	if(t > 0.0) {
+		// Normal vector of sphere.
 		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		// Return rgb color with each component ranging from 0 to 1.
 		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
 	}
 
+	// Handle points that are outside of the sphere. (Background)
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
+	// Gradient background fromula.
 	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
-	// IMAGE
+	// __IMAGE__
 
 	auto aspect_ratio = 16.0 / 9.0;
 	int image_width = 400;
@@ -59,7 +68,7 @@ int main() {
 	int image_height = int(image_width / aspect_ratio);
 	image_height = (image_height < 1) ? 1 : image_height;
 
-	// CAMERA
+	// __CAMERA__
 
 	auto focal_length = 1.0;
 	auto viewport_height = 2.0;
@@ -76,20 +85,21 @@ int main() {
 
 	// Calculate location of upper left pixel.
 	/* 
-		First, we need to find point Q - the upper left point of the viewport.
-		We subtract the (0,0,focal_length) vector from the camera (Not really necessary when camera is at 0,0,0).
+		First, we need to find the upper left point of the viewport.
+		We subtract the (0, 0, focal_length) vector from the camera center (Not really necessary when 
+		camera center is at 0,0,0). 
 		The resulting vector points to the center of the viewport.
-		Then, we subtract the resulting vector with (u_half + v_half). The resulting vector points to Q.
+		Then, we subtract the resulting vector with (u_half + v_half). The final vector points to Q.
 	 */
 	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
 	/* 
 		Next we find the first pixel location. The pixel grid is inset from the viewport edges by half
 		of the pixel-to-pixel distance. This way, the viewport is evenly divided into width x height 
-		identical regions.
+		identical regions. So we sum the inset and the upper left point of the viewport.
 	*/
 	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-	// RENDER
+	// __RENDER__
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -108,6 +118,7 @@ int main() {
 		for(int x = 0; x < image_width; x++) {
 			auto pixel_center = pixel00_loc + (x * pixel_delta_u) + (y * pixel_delta_v);
 			auto ray_direction = pixel_center - camera_center;
+			// Ray originating from camera center, pointing to pixel
 			ray r(camera_center, ray_direction);
 
 			color pixel_color = ray_color(r);
